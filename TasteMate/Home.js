d1197db5b3d607ab2008e27eae91caa42f8cd344
smallex <alexandra.fritzen@tum.de>
2018-05-08 12:15:59 +0900

    Home: Added action on location name click

--- TasteMate/Home.js
+++ TasteMate/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {FlatList} from 'react-native';
 import styles from "./styles";
 import {NavBarCreateObsButton, NavBarProfileButton} from "./NavBarButton";
-import {Observation} from "./Observation";
+import {ObservationScreen} from "./ObservationScreen";
 
 export class HomeScreen extends React.Component {
     static navigationOptions = ({navigation})=> ({
@@ -21,13 +21,13 @@ export class HomeScreen extends React.Component {
         return (
             <FlatList
                       data={[
-                          {key: '12343', value: { userid:213, dishname :'A  reall very long title will it fit what will the layout dor', mypoc:'Donut', location:'Donuteria', rating:6, imageid:'asd', price: '3.99', currency:'USD', description:'Donut come for me if you want trouble...', time:'2 days ago', likes:'200k', cutleries:'5k'}},
-                          {key: '1234', value: { userid:213, dishname :'Mr Frosty', mypoc:'Donut', location:'Donuteria', rating:5, imageid:'asd', price: '3.99', currency:'USD', description:'Donut come for me if you want trouble...', time:'3 days ago', likes:'1', cutleries:'0'}},
-                          {key: '12342', value: { userid:213, dishname :'Carbonarinis', mypoc:'Donut', location:'Donuteria', rating:1, imageid:'asd', price: '3.99', currency:'USD', description:'Donut come for me if you want trouble...', time:'4 days ago', likes:'123m', cutleries:'2m'}},
-                          {key: '12344', value: { userid:213, dishname :'Mr Frosty', mypoc:'Donut', location:'Donuteria', rating:2, imageid:'asd', price: '3.99', currency:'USD', description:'Donut come for me if you want trouble...', time:'10 days ago', likes:'3k', cutleries:'607'}},
-                          {key: '12345', value: { userid:213, dishname :'Madam with the longest title ever imaginable but it is a very important dish so it totally makes sense ya know it is more than four lines long wow', mypoc:'Donut', location:'Donuteria', rating:9, imageid:'asd', price: '3.99', currency:'USD', description:'Donut come for me if you want trouble...', time:'32 days ago', likes:'110', cutleries:'7'}},
+                          {key: '12343', value: { userid:213, dishname :'A  reall very long title will it fit what will the layout dor', mypoc:'Donut', location: 'Yoshinoya', googleMapsId: 'ChIJcfDfiROMGGARlXN3FRTivo8', rating:6, imageid:'asd', price: '3.99', currency:'USD', description:'Donut come for me if you want trouble...', time:'2 days ago', likes:'200k', cutleries:'5k'}},
+                          {key: '1234', value: { userid:213, dishname :'Mr Frosty', mypoc:'Donut',location: 'National Institute of Informatics', googleMapsId: 'ChIJ93oq5RGMGGARDvEMb6UBvlk', rating:5, imageid:'asd', price: '3.99', currency:'USD', description:'Donut come for me if you want trouble...', time:'3 days ago', likes:'1', cutleries:'0'}},
+                          {key: '12342', value: { userid:213, dishname :'Carbonarinis', mypoc:'Donut', location: 'Yoshinoya', googleMapsId: 'ChIJcfDfiROMGGARlXN3FRTivo8', rating:1, imageid:'asd', price: '3.99', currency:'USD', description:'Donut come for me if you want trouble...', time:'4 days ago', likes:'123m', cutleries:'2m'}},
+                          {key: '12344', value: { userid:213, dishname :'Mr Frosty', mypoc:'Donut', location: 'Yoshinoya', googleMapsId: 'ChIJcfDfiROMGGARlXN3FRTivo8', rating:2, imageid:'asd', price: '3.99', currency:'USD', description:'Donut come for me if you want trouble...', time:'10 days ago', likes:'3k', cutleries:'607'}},
+                          {key: '12345', value: { userid:213, dishname :'Madam with the longest title ever imaginable but it is a very important dish so it totally makes sense ya know it is more than four lines long wow', mypoc:'Donut', location: 'Yoshinoya', googleMapsId: 'ChIJcfDfiROMGGARlXN3FRTivo8', rating:9, imageid:'asd', price: '3.99', currency:'USD', description:'Donut come for me if you want trouble...', time:'32 days ago', likes:'110', cutleries:'7'}},
                       ]}
-                      renderItem={({item}) => <Observation item={item.value} nav={this.props.navigation}/>}
+                      renderItem={({item}) => <ObservationScreen item={item.value} nav={this.props.navigation}/>}
             />
         );
     }
--- TasteMate/ObservationScreen.js
+++ TasteMate/ObservationScreen.js
@@ -1,27 +1,40 @@
 import React from "react";
-import {ActionSheetIOS, Alert, FlatList, Image, Platform, Text, TextInput, TouchableOpacity, View} from "react-native";
+import {ActionSheetIOS, Alert, FlatList, Image, Platform, Text, TextInput, TouchableOpacity, View, Linking} from "react-native";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 import {brandContrast, brandLight, brandMain} from "./constants/Colors";
 import styles from "./styles";
 import BottomSheet from 'react-native-bottom-sheet';
 
-export class Observation extends React.Component {
+export class ObservationScreen extends React.Component {
     constructor(props) {
         super(props);
         this._onPressMenuButton = this._onPressMenuButton.bind(this);
         this._onPressMenuDetailButton = this._onPressMenuDetailButton.bind(this);
+        this._onPressLocationText = this._onPressLocationText.bind(this);
     }
 
     _onPressLikeButton() {
-        Alert.alert('You tapped the button!');
+        // TODO: Like action
     }
 
     _onPressCutleryButton() {
-        Alert.alert('You tapped the button!');
+        // TODO: Cutlery action
     }
 
     _onPressSendButton() {
-        Alert.alert('You wanna send off your comment wow');
+        // TODO: Send action
+    }
+
+    _onPressLocationText() {
+        // TODO: Possible maps integration, e.g. using https://github.com/react-community/react-native-maps
+        var url =  'https://www.google.com/maps/search/?api=1&query=' + this.props.item.location + '&query_place_id=' + this.props.item.googleMapsId;
+        Linking.canOpenURL(url).then(supported => {
+            if (supported) {
+                Linking.openURL(url);
+            } else {
+                console.log("Don't know how to open URI: " + url);
+            }
+        });
     }
 
     _onPressMenuButton() {
@@ -64,6 +77,7 @@ export class Observation extends React.Component {
 
     _onPressMenuDetailButton(buttonIndex) {
         if (buttonIndex === 0) {
+            // TODO: Whati s being shared? Link?
             var subject = 'Share';
             var message = 'Where do you want to share this post?';
             var url = '';
@@ -126,7 +140,7 @@ export class Observation extends React.Component {
                                     <Text name={'mypoc'} style={styles.textTitle}> ({this.props.item.mypoc})</Text>
                                 </Text>
                             </View>
-                            <Text name={'location'} style={styles.textSmall}>{this.props.item.location}</Text>
+                            <Text name={'location'} style={styles.textSmall} onPress={this._onPressLocationText}>{this.props.item.location}</Text>
                         </View>
                         <FontAwesome name={'ellipsis-v'} size={25} color={brandContrast} style={styles.containerPadding} onPress={this._onPressMenuButton}/>
                     </View>
@@ -149,18 +163,19 @@ export class Observation extends React.Component {
                     </View>
                     <View name={'description'} style={[styles.containerPadding, styles.bottomLine, {flex: 1, flexDirection:'column'}]}>
                         <Text name={'description'} style={styles.textStandard}>{this.props.item.description}</Text>
+                        {/*TODO: reformat time, likes & cutleries from e.g. 2001 likes to 2k likes, date to 2 days ago etc*/}
                         <Text name={'details'} style={styles.textSmall}>{this.props.item.time} • {this.props.item.likes} likes • {this.props.item.cutleries} cutleries</Text>
                     </View>
                     <FlatList name={'comments'} style={[styles.containerPadding, {flex: 1, flexDirection:'column'}]}
                               data={[
                                   {key: '1', value:{userid:123, imageid:'123', message:'Delicious I bet!'}},
-                                  {key: '2', value:{userid:123, imageid:'123', message:'Mmh, so jelly!'}},
+                                  {key: '2', value:{userid:123, imageid:'123', message:'Mmh, so jelly! Hehe remember the last time we had that together? We were both maybe 10 years old and laughing so hard at everything.. lol!! Good times'}},
                                   {key: '3', value:{userid:123, imageid:'123', message:'Oh my goodness, that truly looks amazing! I wish I was there eating this with you! xoxo'}},
                               ]}
                               renderItem={({item}) =>
                                   <View style={{flex: 1, flexDirection:'row', alignItems: 'center'}}>
                                       <Image name={'userpic'} style={[styles.roundProfileSmall, styles.containerPadding]} resizeMode={'cover'} source={require('./user.jpg')} />
-                                      <Text style={[styles.textStandard, styles.containerPadding]}>{item.value.message}</Text>
+                                      <Text style={[styles.textStandard, styles.containerPadding, {flex: 1}]}>{item.value.message}</Text>
                                   </View>}
                               ListFooterComponent={() =>
                                   <View style={{flex: 1, flexDirection:'row', alignItems: 'center'}}>
@@ -180,4 +195,4 @@ export class Observation extends React.Component {
     }
 }
 
-//TOOD: keyboard reopens when alert dismissed, send action, 3+ comments
+//TODO: keyboard reopens when alert dismissed, 3+ comments
