4252c7a7caa1479b1ccd1382427eeb0eaf6e3bbc
smallex <alexandra.fritzen@tum.de>
2018-05-23 10:53:26 +0900

    Remove react-native-bottom-sheet

--- TasteMate/android/app/src/main/java/com/tastemate/MainApplication.java
+++ TasteMate/android/app/src/main/java/com/tastemate/MainApplication.java
@@ -10,7 +10,6 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.gnet.bottomsheet.RNBottomSheetPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -30,7 +29,6 @@ public class MainApplication extends Application implements ReactApplication {
             new RNCameraPackage(),
             new ReactNativeLocalizationPackage(),
             new VectorIconsPackage()
-          , new RNBottomSheetPackage()
       );
     }
 
