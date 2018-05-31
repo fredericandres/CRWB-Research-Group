652a860129815f0f16cdf3f7d0d8a12783606e6a
smallex <alexandra.fritzen@tum.de>
2018-05-31 10:42:46 +0900

    Revert "Updated dependencies"
    This reverts commit 78ca5ec526c92c5ba2612b1073a036e83b0a86ab.

--- TasteMate/index.js
+++ TasteMate/index.js
@@ -6,7 +6,6 @@ YellowBox.ignoreWarnings([
     'Warning: isMounted(...) is deprecated',
     'Module RCTImageLoader requires main queue setup',
     'Module RNFetchBlob requires main queue setup',
-    'Class RCTCxxModule was not exported',
-    'RCTBridge required dispatch_sync'
+    'Class RCTCxxModule was not exported'
 ]);
 AppRegistry.registerComponent('TasteMate', () => App);
