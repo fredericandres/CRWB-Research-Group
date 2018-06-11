e8c57d8c54c902c0f8759c416ced9718e63d5e6a
smallex <alexandra.fritzen@tum.de>
2018-06-11 19:36:31 +0900

    Add build-issues branch of react native firebase

--- TasteMate/android/app/src/main/java/com/tastemate/MainApplication.java
+++ TasteMate/android/app/src/main/java/com/tastemate/MainApplication.java
@@ -3,6 +3,7 @@ package com.tastemate;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import cl.json.RNSharePackage;
 import cl.json.ShareApplication;
 import com.RNFetchBlob.RNFetchBlobPackage;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication, Sh
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
             new RNSharePackage(),
             new RNFetchBlobPackage(),
             new RNCameraPackage(),
